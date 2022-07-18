86e45dc29990b43ef924e2e9fb8f228efda7c222
Kyle VanTassell <octicon@gmail.com>
2022-07-17 19:03:13 -0500

    monthly

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 console.log('js');
 
+
 $(readyNow);
 
 function readyNow() {
@@ -8,30 +9,39 @@ function readyNow() {
     $('#delete').on('click', removeEmp);
     $('body').on('click', '#delete', removeEmp);
 }
+let firstName;
+let lastName;
+let idNumber;
+let jobTitle;
+let annualIncome;
+totalMonthly = 0;
+
+
 
 // creating function to add emp data to table, clear fields
+
 function submitEmployee() {
     // console.log('clicked button');
     // naming variables 
-    let firstName = $('#first-name').val();
-    let lastName = $('#last-name').val();
-    let idNumber = $('#id-number').val();
-    let jobTitle = $('#title').val();
-    let annualIncome = $('#annual-salary').val();
+    firstName = $('#first-name').val();
+    lastName = $('#last-name').val();
+    idNumber = $('#id-number').val();
+    jobTitle = $('#title').val();
+    annualIncome = Number($('#annual-salary').val());
+    // $('#total-monthly').append(totalmonth / 12); //not correct, concatinates .val()
+    appendTotalMonthly();
 
     // append to table
     $('#employee-data').append(`
-    <tr>
-        <td>${firstName}</td>
-        <td>${lastName}</td>
-        <td>${idNumber}</td>
-        <td>${jobTitle}</td>
-        <td>${annualIncome}</td>
-        <td><button id="delete">Delete</button></td>
-    `)
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>$${annualIncome}</td>
+            <td><button id="delete">Delete</button></td>
+            `)
     $('input').val('');
-
-
 }
 
 // remove employee from table
@@ -39,6 +49,22 @@ function removeEmp() {
     // console.log('remove employee');
     $(this).parent().parent().remove();
 }
+
+function appendTotalMonthly() {
+    console.log('in totalMonthly');
+    console.log(annualIncome);
+    totalMonthly += annualIncome / 12;
+    console.log(totalMonthly);
+    $('#total-monthly').text('');
+    $('#total-monthly').text(`Total Monthly: $${totalMonthly}`);
+
+
+
+}
+
+
+// wirte a funciton that takes in ${annualIncome}, adds it together, and devides by 12
+
 
 
 
